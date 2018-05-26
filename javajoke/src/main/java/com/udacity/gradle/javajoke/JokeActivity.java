783acba9e4b71442e6d7fb6ab756e189033d1434
Amir Mattar <amir86mattar@gmail.com>
2018-05-26 22:36:48 +0300

    Adding Navigation bar and implementing the back button

--- javajoke/src/main/java/com/udacity/gradle/javajoke/JokeActivity.java
+++ javajoke/src/main/java/com/udacity/gradle/javajoke/JokeActivity.java
@@ -1,7 +1,9 @@
 package com.udacity.gradle.javajoke;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
@@ -13,8 +15,24 @@ public class JokeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
 
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
         TextView textViewJoke = findViewById(R.id.jokeText);
         joke = getIntent().getStringExtra(JOKE);
         textViewJoke.setText(joke);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
